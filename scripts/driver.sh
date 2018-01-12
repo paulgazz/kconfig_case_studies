c77bb7a4b426c3eea3780f157847bec03e807e90
Paul <gazzillo@cs.yale.edu>
2018-01-12 13:32:39 -0500

    combine dimacs generation, configuring, and building in to the same script.  add a list option to see available cases.

--- scripts/driver.sh
+++ scripts/driver.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-if [[ $# -lt 3 && $1 != "dimacs" || $# -lt 2 && $1 == "dimacs" ]]; then
-    echo "USAGE: $(basename $0) config|build casename config_file"
+if [[ $# -lt 1 ]]; then
+    echo "USAGE: $(basename $0) action casename"
     echo ""
-    echo "  This configures or builds the project for each sampled configuration"
+    echo "   action may be one of the following"
+    echo "     dimacs - generate dimacs file"
+    echo "     config - configure the system for each sampled config"
+    echo "     build  - build the system for each sampled config"
     echo ""
-    echo "    config|build case subdir in the repo"
-    echo "    casename     case subdir in the repo"
-    echo "    config_file  relative path to config file in current project"
+    echo "    casename - the subdirectory of the case in the kconfig_case_studies repo"
     echo ""
-    echo "USAGE: $(basename $0) dimacs casename"
+    echo "USAGE: $(basename $0) list"
     echo ""
-    echo "  This collects the dimacs version of the Kconfig constraints"
-    echo ""
-    echo "    casename     case subdir in the repo"
+    echo "  list the active cases"
     echo ""
     echo "NOTES:"
     echo "  - Run this script from root of a project's source directory."
     echo "  - Be sure that KCONFIG_CASE_STUDIES has been set to the repo."
-    echo "  - For 'collect' be sure that KMAX_ROOT has been set to the kmax repo."
+    echo "  - For 'dimacs' be sure that KMAX_ROOT has been set to the kmax repo."
     exit 1
 fi
 
@@ -30,35 +29,67 @@ fi
 
 action="${1}"
 casename="${2}"
-config_file="${3}"
 
 case_dir="${KCONFIG_CASE_STUDIES}/cases/${casename}"
 
 if [[ ! -e "${case_dir}" ]]; then
     echo "${case_dir} does not exist."
+    action="list"
+fi
+
+if [[ "${casename}" == "" || "${action}" == "list" ]]; then
     echo "Please choose from the following:"
     ls ${KCONFIG_CASE_STUDIES}/cases/
     exit 1
 fi
 
-if [[ "${action}" == "config" ]]; then
-    for i in ${case_dir}/Configs/*.config; do
-      echo "configuring $i";
-      cat $i | grep -v "SPECIAL_ROOT_VARIABLE" > "${config_file}";
-      make oldconfig;
-      "${KCONFIG_CASE_STUDIES}/scripts/compare_configs.sh" "${i}" "${config_file}";
-      echo "diff result: ${?}";
-    done 2>&1 | tee "${case_dir}/config_diff_results.out"
 
-elif [[ "${action}" == "build" ]]; then
-    for i in ${case_dir}/Configs/*.config; do
-      echo "configuring $i";
-      cat $i | grep -v "SPECIAL_ROOT_VARIABLE" > "${config_file}";
-      make oldconfig;
-      echo "building $i";
-      make;
-      echo "return code $?";
-    done 2>&1 | tee "${case_dir}/build_results.out"
+# setup case-specific properties
+config_file=""
+kconfig_root=""
+echo "${casename}" | grep -i "axtls" > /dev/null
+if [[ $? -eq 0 ]]; then
+    config_file="config/.config"
+    kconfig_root="config/Config.in"
+fi
+echo "${casename}" | grep -i "toybox" > /dev/null
+if [[ $? -eq 0 ]]; then
+    config_file=".config"
+    kconfig_root="Config.in"
+fi
+echo "${casename}" | grep -i "fiasco" > /dev/null
+if [[ $? -eq 0 ]]; then
+    config_file="build/globalconfig.out"
+    kconfig_root="build/Kconfig"
+fi
+
+if [[ "${config_file}" == "" || "${kconfig_root}" == "" ]]; then
+    echo "unknown case '${casename}. please add it to this script '${0}'"
+    exit 1
+fi
+
+
+if [[ "${action}" == "config" || "${action}" == "build" ]]; then
+    # configure or build each sample
+    if [[ "${action}" == "config" ]]; then
+        for i in ${case_dir}/Configs/*.config; do
+          echo "configuring $i";
+          cat $i | grep -v "SPECIAL_ROOT_VARIABLE" > "${config_file}";
+          make oldconfig;
+          "${KCONFIG_CASE_STUDIES}/scripts/compare_configs.sh" "${i}" "${config_file}";
+          echo "diff result: ${?}";
+        done 2>&1 | tee "${case_dir}/config_diff_results.out"
+
+    elif [[ "${action}" == "build" ]]; then
+        for i in ${case_dir}/Configs/*.config; do
+          echo "configuring $i";
+          cat $i | grep -v "SPECIAL_ROOT_VARIABLE" > "${config_file}";
+          make oldconfig;
+          echo "building $i";
+          make;
+          echo "return code $?";
+        done 2>&1 | tee "${case_dir}/build_results.out"
+    fi
 
 elif [[ "${action}" == "dimacs" ]]; then
     if [[ ! -e "${KMAX_ROOT}" ]]; then
@@ -68,17 +99,18 @@ elif [[ "${action}" == "dimacs" ]]; then
     fi
 
     # axtls variables already include the the CONFIG_ prefix and it's
-    # kconfig system is modified not to.
-    echo "${casename}" | grep -i "axtls"
-    echo $?
+    # kconfig system is modified not to.  add a flag to check_dep to
+    # disable the prefix for axtls.
+    echo "${casename}" | grep -i "axtls" >/dev/null
     if [[ $? -eq 0 ]]; then
         extra_args="-p"
     else
       extra_args=""
     fi
 
-    "${KMAX_ROOT}/kconfig/check_dep" ${extra_args} --dimacs config/Config.in | tee "${case_dir}/${casename}.kmax" | python "${KMAX_ROOT}/kconfig/dimacs.py" > "${case_dir}/${casename}.dimacs"
+    # get the dimacs file by running kmax's check_dep
+    "${KMAX_ROOT}/kconfig/check_dep" ${extra_args} --dimacs "${kconfig_root}" | tee "${case_dir}/${casename}.kmax" | python "${KMAX_ROOT}/kconfig/dimacs.py" > "${case_dir}/${casename}.dimacs"
 
     # without reverse dependencies
-    "${KMAX_ROOT}/kconfig/check_dep" ${extra_args} -D --dimacs config/Config.in | tee "${case_dir}/${casename}_sans_reverse.kmax" | python "${KMAX_ROOT}/kconfig/dimacs.py" > "${case_dir}/${casename}_sans_reverse.dimacs"
+    "${KMAX_ROOT}/kconfig/check_dep" ${extra_args} -D --dimacs "${kconfig_root}" | tee "${case_dir}/${casename}_sans_reverse.kmax" | python "${KMAX_ROOT}/kconfig/dimacs.py" > "${case_dir}/${casename}_sans_reverse.dimacs"
 fi
