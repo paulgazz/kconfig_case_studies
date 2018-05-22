503b1f0cd7c26737e879bee580c87ea169db8888
Paul <github@pgazz.com>
2018-05-21 22:08:38 -0400

    fix path name

--- scripts/run_config.sh
+++ scripts/run_config.sh
@@ -3,4 +3,4 @@
 (cd /home/vagrant/axtls-code && /vagrant/scripts/driver.sh config axtls_2_1_4)
 (cd /home/vagrant/toybox-0.7.5 && /vagrant/scripts/driver.sh config toybox_0_7_5)
 (cd /home/vagrant/busybox-1.28.0 && yes '' | /vagrant/scripts/driver.sh config busybox_1_28_0)
-(cd /home/vagrant/fiasco-17.10/src/kernel/fiasco && yes '' | /vagrant/scripts/driver.sh config fiasco_17_10)
+(cd /home/vagrant/fiasco_17_10/src/kernel/fiasco && yes '' | /vagrant/scripts/driver.sh config fiasco_17_10)
