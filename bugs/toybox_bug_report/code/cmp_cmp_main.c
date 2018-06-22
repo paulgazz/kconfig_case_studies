26495181a9bf043de010e066f778bf3d7a410361
Austin Mordahl <austin.mordahl@utdallas.edu>
2018-06-22 17:28:02 -0500

    Added line numbers and a new bug to toybox bug report.

--- bugs/toybox_bug_report/code/cmp_cmp_main.c
+++
@@ -1,7 +0,0 @@
-void cmp_main(void)
-{
-  toys.exitval = 2;
-  loopfiles_rw(toys.optargs,
-	       O_CLOEXEC|(WARN_ONLY*!(toys.optflags&FLAG_s)),
-	       0, do_cmp);
-}
--- bugs/toybox_bug_report/code/uudecode_uudecode_main.c
+++
@@ -1,9 +0,0 @@
-void uudecode_main(void)
-{
-  int ifd = 0, ofd, idx = 0, m = m;
-  char *line = 0, mode[16],
-       *class[] = {"begin%*[ ]%15s%*[ ]%n",
-		   "begin-base64%*[ ]%15s%*[ ]%n"};
-
-  // ...
-}
