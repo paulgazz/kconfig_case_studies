a1764c6defa22805c248ae762c3edf9ac7ca081e
Paul <github@pgazz.com>
2018-07-16 20:53:59 -0400

    remove all old js versions of post-processing scripts

--- bugs/cppcheck/scripts/combine_per_config_bugs.sh
+++
@@ -1,6 +0,0 @@
-# This program takes the results of run_cppcheck.sh and collects them
-# into a single file per configurationcalled cppcheck_all.txt.
-
-for i in *.config; do
-  find -name "*.i.cppcheck" | xargs cat | grep "^\[" > ${i}/cppcheck_all.txt;
-done
