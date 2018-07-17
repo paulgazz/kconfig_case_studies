019e57977405f60ee1a56ae42954d9e14426ca5a
Paul <github@pgazz.com>
2018-07-17 15:01:45 -0400

    Identify binaries for Linux to be used to measure build size.

--- scripts/driver.sh
+++ scripts/driver.sh
@@ -145,7 +145,7 @@ echo "${casename}" | grep -i "linux" > /dev/null
 if [[ $? -eq 0 ]]; then
     config_file=".config"
     kconfig_root="Kconfig"
-    binaries="vmlinux"  # TODO: set binaries
+    binaries="arch/x86/boot/bzImage"  # TODO: set binaries
     check_dep_extra_args="-p -e SRCARCH=x86"  # run on x86 version of Linux
     touch .br2-external.in  # this file is necessary in order to process the Config.in
 fi
