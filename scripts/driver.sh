0d03249baecb707be30f3862e2b764509c3cfbd5
Paul <github@pgazz.com>
2018-07-20 13:16:09 -0400

    Provide support for preprocessing the Linux kernel as well as Fiasco.  For fiasco, a modified version of the preprocess_config.py script called preprocess_config_fiasco.py was necessary, since it does not produce the same Makefile logs as a typical Kbuild build system.

--- scripts/driver.sh
+++ scripts/driver.sh
@@ -227,57 +227,81 @@ if [[ "${action}" == "config" || "${action}" == "build" || "${action}" == "prepr
             print "unknown action for compilation"
             exit 1
           fi
-          
-          for dummy in $(seq 1 1); do  # single-iteration loop to make saving output easier
-            echo "${casename}" | grep -i "linux" > /dev/null
-            if [[ $? -eq 0 ]]; then
-                # complete cleanup for linux
-                make mrproper
-            fi
-            
-            echo "configuring $i";
-            cat $i | grep -v "SPECIAL_ROOT_VARIABLE" > "${config_file}";
 
-            # case-specific build scripts
-            echo "${casename}" | grep -i "axtls" > /dev/null
-            if [[ $? -eq 0 ]]; then
-                mkdir -p /tmp/lua
-                echo 'CONFIG_HTTP_LUA_PREFIX="/tmp/lua"' >> "${config_file}";
-            fi
-            echo "${casename}" | egrep -i "uClibc-ng" >/dev/null
-            if [[ $? -eq 0 ]]; then
-                mv "${config_file}" "${config_file}.tmp"
-                cat "${config_file}.tmp" | grep -v "^KERNEL_HEADERS=" > "${config_file}"; echo 'KERNEL_HEADERS="/home/vagrant/linux-headers/include"' >> "${config_file}"
-            fi
+          if [[ -e "${save_file}" || -e "${save_file}.bz2" ]]; then
+              echo "skipping existing build: ${save_file}"
+          else
+            for dummy in $(seq 1 1); do  # single-iteration loop to make saving output easier
+              echo "${casename}" | grep -i "linux" > /dev/null
+              if [[ $? -eq 0 ]]; then
+                  # complete cleanup for linux
+                  make mrproper
+              fi
+              
+              echo "configuring $i";
+              cat $i | grep -v "SPECIAL_ROOT_VARIABLE" > "${config_file}";
 
-            time make oldconfig;
+              # case-specific build scripts
+              echo "${casename}" | grep -i "axtls" > /dev/null
+              if [[ $? -eq 0 ]]; then
+                  mkdir -p /tmp/lua
+                  echo 'CONFIG_HTTP_LUA_PREFIX="/tmp/lua"' >> "${config_file}";
+              fi
+              echo "${casename}" | egrep -i "uClibc-ng" >/dev/null
+              if [[ $? -eq 0 ]]; then
+                  mv "${config_file}" "${config_file}.tmp"
+                  cat "${config_file}.tmp" | grep -v "^KERNEL_HEADERS=" > "${config_file}"; echo 'KERNEL_HEADERS="/home/vagrant/linux-headers/include"' >> "${config_file}"
+              fi
 
+              time make oldconfig;
+
+              if [[ "${action}" == "preprocess" ]]; then
+                  # emit entire gcc commands
+                  echo "${casename}" | egrep -i "fiasco" >/dev/null
+                  if [[ $? -eq 0 ]]; then
+                      # fiasco's flag for verbose mode
+                      preprocess_args_opt="V=1"
+                  else
+                    # kbuild-based build system flag for verbose mode
+                    preprocess_args_opt="KBUILD_VERBOSE=1"
+                  fi
+              else
+                preprocess_args_opt=""
+              fi
+
+              echo "building $i";
+              make clean;
+              echo "${casename}" | grep -i "axtls" > /dev/null
+              if [[ $? -eq 0 ]]; then
+                  mkdir -p /tmp/local
+                  time make ${preprocess_args_opt} ${make_extra_args} PREFIX="/tmp/local"
+              else
+                time make ${preprocess_args_opt} ${make_extra_args};
+              fi
+              echo "return code $?";
+              echo "binary size (in bytes): $(du -bc ${binaries} | tail -n1 | cut -f1)"
+            done 2>&1 | tee "${save_file}.tmp" | egrep "^(building)"
+            mv ${save_file}.tmp ${save_file}
+            bzip2 -f "${save_file}"
+          fi
+
+          if [[ -e "${preprocess_out_file}" || -e "${preprocess_out_file}.bz2" ]]; then
+              echo "skipping existing preprocess: ${save_file}"
+          else
             if [[ "${action}" == "preprocess" ]]; then
-                preprocess_args_opt="KBUILD_VERBOSE=1" # emit entire gcc commands
-            else
-              preprocess_args_opt=""
+                echo "preprocessing $i"
+                bunzip2 -f "${save_file}.bz2"
+                echo "${casename}" | egrep -i "fiasco" >/dev/null
+                if [[ $? -eq 0 ]]; then
+                    preprocess_script="preprocess_config_fiasco.py"
+                else
+                  preprocess_script="preprocess_config.py"
+                fi
+                python "${KCONFIG_CASE_STUDIES}/scripts/${preprocess_script}" "${save_file}" "${preprocessed_outdir}/${i_base}" > "${preprocess_out_file}.tmp" 2>&1
+                bzip2 "${save_file}"
+                mv ${preprocess_out_file}.tmp ${preprocess_out_file}
+                bzip2 -f "${preprocess_out_file}"
             fi
-
-            echo "building $i";
-            make clean;
-            echo "${casename}" | grep -i "axtls" > /dev/null
-            if [[ $? -eq 0 ]]; then
-                mkdir -p /tmp/local
-                time make ${preprocess_args_opt} ${make_extra_args} PREFIX="/tmp/local"
-            else
-              time make ${preprocess_args_opt} ${make_extra_args};
-            fi
-            echo "return code $?";
-            echo "binary size (in bytes): $(du -bc ${binaries} | tail -n1 | cut -f1)"
-          done 2>&1 | tee "${save_file}" | egrep "^(building)"
-
-          bzip2 -f "${save_file}"
-          if [[ "${action}" == "preprocess" ]]; then
-              echo "preprocessing $i"
-              bunzip2 -f "${save_file}.bz2"
-              python "${KCONFIG_CASE_STUDIES}/scripts/preprocess_config.py" "${save_file}" "${preprocessed_outdir}/${i_base}" > "${preprocess_out_file}" 2>&1
-              bzip2 "${save_file}"
-              bzip2 -f "${preprocess_out_file}"
           fi
         done
     fi
