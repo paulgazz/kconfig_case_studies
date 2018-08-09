e722643f7d7949a1cec870905e91508310eaaadd
Austin Mordahl <austin.mordahl@utdallas.edu>
2018-08-09 18:42:12 -0500

    Updated scripts and axtls results for cbmc.

--- bugs/ikos/scripts/ikos_setup.sh
+++ bugs/ikos/scripts/ikos_setup.sh
@@ -4,4 +4,5 @@ export LLVM_CC_NAME=clang-4.0
 export LLVM_LINK_NAME=llvm-link-4.0
 export LLVM_AR_NAME=llvm-ar-4.0
 export CC=wllvm
+export HOSTCC=wllvm
 export CFLAGS="-Wall -Wundef -Wno-char-subscripts -Werror=implicit-function-declaration -g"
