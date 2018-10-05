005b351b4e0f1c8ff33e4d9844812880ad08c99e
Paul <github@pgazz.com>
2018-10-05 15:18:36 -0400

    Better error handling when processing summary data.

--- bugs/cppcheck/scripts/resolve_lines.py
+++ bugs/cppcheck/scripts/resolve_lines.py
@@ -28,6 +28,7 @@ for config_dir in natsort.natsorted(glob.iglob('*.config', recursive=False)):
               old_line = int(result.group(4))
               ref_file = old_file
               ref_line = 1
+              found = False
               with open(old_file) as g:
                 g_i = 0
                 for line in g:
@@ -36,12 +37,17 @@ for config_dir in natsort.natsorted(glob.iglob('*.config', recursive=False)):
                   ref_line += 1
                   if line.startswith("# "): # a preprocessor output line ref
                     split_line_ref = line.split()
-                    # 0 is the hash
-                    ref_line = int(split_line_ref[1]) - 1 # substract one b/c the _next_ line in the .i file is the referenced line
-                    ref_file = split_line_ref[2][1:-1] # remove quotes
+                    # 0 is the hash symbol
+                    try:
+                      ref_line = int(split_line_ref[1]) - 1 # substract one b/c the _next_ line in the .i file is the referenced line
+                      ref_file = split_line_ref[2][1:-1] # remove quotes
+                    except:
+                      sys.stderr.write("could not resolve %s from %s\n" % (line, cppcheck_result))
                     # 3 and above are flags not relevant for line resolution
                   if (g_i == old_line):
                     # sys.stderr.write(("translating %s:%s to %s:%s\n" % (old_file, old_line, ref_file, ref_line)))
+                    found = True
                     break
-              new_error = "%s%s%s%d%s" % (result.group(1), ref_file, result.group(3), ref_line, result.group(5))
-              outfile.write("%s\n" % new_error)
+              if found:
+                new_error = "%s%s%s%d%s" % (result.group(1), ref_file, result.group(3), ref_line, result.group(5))
+                outfile.write("%s\n" % new_error)
